78feb1a5ce8154a5af5d6bfc4dd87b7216e0c62e
Nikola Martić <nikola.martic@gmail.com>
2019-04-04 13:30:52 +0200

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Person.java
@@ -1,14 +1,20 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
 
+    @Column(name = "first_name")
     public String getFirstName() {
         return firstName;
     }
 
+    @Column(name = "last_name")
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
