1bf6f617c34e36249d9db14643f014c9e8161080
Nikola Martić <nikola.martic@gmail.com>
2019-04-04 14:21:53 +0200

    Implemented Spring Data JPA Repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package com.example.sfgpetclinic.repositories;
+
+import com.example.sfgpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Speciality, Long> {
+
+}
